0b762d7fa0f92d5a52407aa20c9bcce6b4540d52
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-04-23 22:58:44 +0200

    Scaffold vNext application via aspnet generator

---
+++ after/Compiler/Preprocess/RazorPreCompilation.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.AspNet.Mvc;
+
+namespace after.Compiler.Preprocess
+{
+    public class RazorPreCompilation : RazorPreCompileModule
+    {
+        public RazorPreCompilation(IServiceProvider provider) : base(provider)
+        {
+        }
+    }
+}
